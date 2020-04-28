3d025a6ff469a180ae7f71b046af5e4bad7baff3
824750130 <e824750130@qq.com>
2020-04-28 17:35:55 +0800

    modify python fc util code style

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -1,29 +1,29 @@
 import unittest
 
-from fc_util.client import Client
+from fc_util import client
 from Tea.request import TeaRequest
 
 
 class TestClient(unittest.TestCase):
 
     def test_get_content_md5(self):
-        self.assertEqual("CY9rzUYh03PK3k6DJie09g==", Client.get_content_md5(
+        self.assertEqual("CY9rzUYh03PK3k6DJie09g==", client.get_content_md5(
             bytes("test", encoding="utf-8")))
 
     def test_get_content_length(self):
-        self.assertEqual(4, Client.get_content_length(
+        self.assertEqual(4, client.get_content_length(
             bytes("test", encoding="utf-8")))
 
     def test_use(self):
-        self.assertEqual("a", Client.use(True, "a", "b"))
-        self.assertEqual("b", Client.use(False, "a", "b"))
-        self.assertEqual("b", Client.use(None, "a", "b"))
+        self.assertEqual("a", client.use(True, "a", "b"))
+        self.assertEqual("b", client.use(False, "a", "b"))
+        self.assertEqual("b", client.use(None, "a", "b"))
 
     def test_get_signature(self):
         request = TeaRequest()
         request.pathname = "/test"
         request.method = "GET"
-        signedStr = Client.get_signature(
+        signedStr = client.get_signature(
             "accessKeyId", "accessKeySecret", request, "/version")
         self.assertEqual(
             "FC accessKeyId:tvvm2KiUDmXlQiuwz21CtN59JcKTT5QzIcrc122DKVo=", signedStr)
@@ -31,7 +31,7 @@ class TestClient(unittest.TestCase):
         header = {}
         header["x-fc-test"] = "test"
         request.headers = header
-        signedStr = Client.get_signature(
+        signedStr = client.get_signature(
             "accessKeyId", "accessKeySecret", request, "/version")
         self.assertEqual(
             "FC accessKeyId:lP6dXPCXlQNI7VY+f0brnZ4bwluNvGqcwdDf5GoM9ag=", signedStr)
@@ -40,7 +40,7 @@ class TestClient(unittest.TestCase):
         query["testQuery"] = "test"
         request.query = query
         request.pathname = "/version/proxy/"
-        signedStr = Client.get_signature(
+        signedStr = client.get_signature(
             "accessKeyId", "accessKeySecret", request, "/version")
         self.assertEqual(
             "FC accessKeyId:aCD88kzuMQYKQGo7kq7DeqhT72nVrNSEqAJtiHNj9L0=", signedStr)
